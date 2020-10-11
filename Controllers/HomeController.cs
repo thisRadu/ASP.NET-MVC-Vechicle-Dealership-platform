f13743dafc0c7f4281dad3c42814d1a4158d4e7b
radu9502 <65829311+radu9502@users.noreply.github.com>
2020-10-12 00:23:38 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebApplication2.Models;
+using Bazar.Models;
+
+
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+      
+        public IActionResult DeleteCar(int id)
+        {
+            Main.RemoveCar(id);
+            return RedirectToAction("Index");
+        }
+        public IActionResult SellCar()
+        {
+            return View();
+        }
+    
+        [HttpPost]
+        public IActionResult SellCar(CarModel car)
+        {
+          
+            DatabaseOperation.InsertCar(car);
+            return RedirectToAction("Index");
+        }
+        public IActionResult CarDetails(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+         
+
+        }
+     
+   
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
